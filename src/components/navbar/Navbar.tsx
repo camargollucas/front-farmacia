2d5de520185720de0a001a85e18e25d91116d253
Lucas <camargolimalucas@gmail.com>
2024-02-28 12:53:23 -0300

    pequenas melhorias

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { HouseSimple, PlusSquare, List, Pill } from '@phosphor-icons/react';
+import './Nabvar.css';
 
 function Navbar() {
   const navigate = useNavigate();
@@ -7,13 +9,31 @@ function Navbar() {
     <>
       <div className='w-full bg-sky-900 text-white flex justify-center py-4'>
         <div className="container flex justify-between text-lg">
-          <div className='text-2xl font-bold uppercase'>Farmácia Paracetamal</div>
+          <div className='logo nav-item gap-2'>
+            <Pill size={28}></Pill>
+            <div className='text-2xl font-bold uppercase'>Farmácia Paracetamal</div>
+          </div>
 
-          <div className='flex gap-4'>
-            <Link to='/home' className='hover:underline'>Home</Link>
-            <div className='hover:underline'>Produtos</div>
-            <Link to='/categorias' className='hover:underline'>Categorias</Link>
-            <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
+          <div className='nav-links flex gap-7'>
+            <div className='nav-item gap-2'>
+              <HouseSimple size={18}></HouseSimple>
+              <Link to='/home' className='hover:underline'>Home</Link>
+            </div>
+
+            <div className='nav-item gap-2' >
+              <List size={18}></List>
+              <div className='hover:underline'>Produtos</div>
+            </div>
+
+            <div className='nav-item gap-2'>
+              <List size={18}></List>
+              <Link to='/categorias' className='hover:underline'>Categorias</Link>
+            </div>
+
+            <div className='nav-item gap-2'>
+              <PlusSquare size={18}></PlusSquare>
+              <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
+            </div>
           </div>
         </div>
       </div>
